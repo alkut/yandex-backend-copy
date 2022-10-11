b4aba00b454164d95c3b5d33dbc0e0f0ab55ea0c
alkut <alexseykutasov@gmail.com>
2022-10-11 22:55:46 +0300

    nothing is done

---
+++ UrlResolve/UrlParametersContainer.cpp
@@ -0,0 +1,13 @@
+#include "UrlParametersContainer.h"
+
+RequestParameters<RequestType::Import>::RequestParameters(const string &s)
+{
+
+}
+
+void RequestParameters<RequestType::Import>::Validate(CodeMessage &result)
+{
+
+}
+
+
