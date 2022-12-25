76d84ed4d2b91eb25020761ac98ff3d68daa3e28
alkut <alexseykutasov@gmail.com>
2022-12-25 03:13:14 +0300

    YAS-2-load-teamcity-build-configuration

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,9 +4,9 @@ TEST(Test, Positive) {
     ASSERT_EQ(0, 0);
 }
 
-TEST(Test, Negative) {
-    ASSERT_EQ(1.0, 0.0);
-}
+//TEST(Test, Negative) {
+//    ASSERT_EQ(1.0, 0.0);
+//}
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
