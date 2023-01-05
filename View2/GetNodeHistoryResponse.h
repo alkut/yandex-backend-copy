5f33b2c890ace3bebeb2693338fd41d1c0658dc6
alkut <alexseykutasov@gmail.com>
2023-01-06 00:57:46 +0300

    feature/YAS-14-get-rid-of-recursion-in-filesyste

--- View2/GetNodeHistoryResponse.h
+++
@@ -1,8 +0,0 @@
-#ifndef YAD_GETNODEHISTORYRESPONSE_H
-#define YAD_GETNODEHISTORYRESPONSE_H
-
-#include "UpdateResponse.h"
-
-typedef UpdateResponse GetNodeHistoryResponse;
-
-#endif //YAD_GETNODEHISTORYRESPONSE_H
