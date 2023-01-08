071ffa2e8463e0d5d27a5115c251ffc14ec08e6d
Igor Krasikov <krasikovir@gmail.com>
2023-01-08 23:55:30 +0500

    YAS-12-add-application-template-class update CMakeLists.txt – add libcurl package update Dockerfile – reduce number fo RUNS update bitbucket-pipelines.yml – run tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -9,6 +9,7 @@
 >>>>>>> fc06ffd (YAS-12-add-application-template-class)
 =======
 #include "../application/EchoServer.hpp"
+#include <curl/curl.h>
 #include "../Configuration.h"
 >>>>>>> ba6314f (YAS-12-add-application-template-class)
 
@@ -26,6 +27,7 @@ TEST(echo_server, start) {
 
 TEST(echo_server, start) {
     Application <EchoServer> app;
+    // app.run();
 }
 
 int main(int argc, char **argv) {
