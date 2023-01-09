8f71e4b36cd558fde74f06290063eba347556acc
alkut <alexseykutasov@gmail.com>
2023-01-09 04:55:12 +0300

    feature/YAS-21-change-project-structure

--- src/Configuration.cpp
+++ src/Configuration.cpp
@@ -5,10 +5,4 @@ void InitLogging(char **argv)
     FLAGS_logtostderr = false;
     FLAGS_timestamp_in_logfile_name = false;
     google::InitGoogleLogging(argv[0]);
-    if (std::strcmp(argv[1], "test\n")) {
-        google::SetLogDestination(0, "../tests/Logs/test.log\n");
-    }
-    else {
-        google::SetLogDestination(0, logging_path.c_str());
-    }
 }
