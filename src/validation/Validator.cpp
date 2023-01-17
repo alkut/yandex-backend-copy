96b063d8a210622f9c0e63323cf7a38f154537d1
alkut <alexseykutasov@gmail.com>
2023-01-17 18:34:26 +0300

    YAS-16-add-validator-class

--- src/validation/Validator.cpp
+++ src/validation/Validator.cpp
@@ -15,6 +15,7 @@ long long Validator::check_datetime(const string &date) {
     string s = date;
     std::replace(s.begin(), s.end(), 'T', ' ');
     s.resize(std::remove(s.begin(), s.end(), 'Z') - s.begin());
+    boost::algorithm::replace_all(s, "%3A", ":");
     auto delta = boost::posix_time::time_from_string(s) - origin;
     return static_cast<long long>(delta.total_milliseconds());
 }
@@ -30,15 +31,23 @@ void Validator::check_count_sub_url(const QueryExt &query, const size_t count) {
 ImportBodyMessage Validator::GetImport(const QueryExt &query) {
     auto body = ImportBodyMessage();
     body.Deserialize(nlohmann::json::parse(query.body));
+    for (const auto& item: body.Items) {
+        if (item._systemItemType == SystemItemType::FILE && item.size <= 0)
+            throw std::invalid_argument("empty file");
+        if (item._systemItemType == SystemItemType::FOLDER && item.size != 0)
+            throw std::invalid_argument("not empty folder");
+        if (item.url.size() > 255)
+            throw std::invalid_argument("too big url");
+    }
     return body;
 }
 
 DeleteItem Validator::GetDelete(const QueryExt &query) {
-    return {query.parsed_url[1], query.params.at("date")};
+    return {query.parsed_url[2], query.params.at("date")};
 }
 
 string Validator::GetNodes(const QueryExt &query) {
-    return query.parsed_url[1];
+    return query.parsed_url[2];
 }
 
 string Validator::GetUpdate(const QueryExt &query) {
